2809ff2b7943c4075e45d51a87b629be6800d7b0
Akash Shinde <saakashs1999@gmail.com>
2024-06-03 02:25:26 +0530

    updated

--- Terraform/eks/main.tf
+++ Terraform/eks/main.tf
@@ -85,9 +85,10 @@ resource "aws_eks_node_group" "eks_node_group" {
   }
 }
 
-output "eks_worker_node_ip" {
-  value = [aws_eks_node_group.eks_node_group.node_public_ip]
+output "elastic_ip" {
+  value = aws_eip.eks_node_eip.public_ip
 }
+
 
 
 provider "kubernetes" {
